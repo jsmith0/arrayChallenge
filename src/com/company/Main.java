14ba1349ec00f31448283413cf845fd7cb9a5d7a
jsmith0 <justins4@comcast.net>
2016-10-27 20:43:07 -0400

    Justin Smith ArrayChallenge.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,42 +4,42 @@ import java.util.*;
 
 public class Main {
 
-
     public static void main(String[] args) {
-
-
-        //this is not dynamic code
-
-        /*
-        int grades [] = {90, 95, 100};
-            int sum = grades[0] + grades [1] + grades [2];
-            double avg = sum/3;
-        System.out.println(avg);
-        */
-
-
-        //this is dynamic code
-        System.out.println("Enter your grades... type -1 to break it");
-        Scanner kb = new Scanner(System.in);
-        int grade = kb.nextInt();
-        int i = 0;
-        int sum = 0;
-        int count = 0;
         int grades[] = new int[1000];
-
-
+        Scanner input = new Scanner(System.in);
+        int grade;
+        int count = 0;
+        int sum = 0;
         while (true) {
-            if (grade == -1) {
+            System.out.println("Enter a grade or \"-1\" to see your grade.");
+            grade = input.nextInt();
+            if (grade == -1)
                 break;
-            }
-            grades[i] = grade;
-            sum = sum + grades[i];
-            i++;
-            count++;
+            grades[count] = grade;
 
-            double average = sum / count;
-            System.out.println(average);
+            sum = sum + grades[count];
+            count++;
         }
 
+        int average = sum / count;
+        System.out.println("Your average is: " + average);
+
+        String names[] = new String [1000];
+        Scanner input2 = new Scanner(System.in);
+        String name;
+        int count2 = 0;
+        int sum2 = 0;
+        while(true) {
+            System.out.println("Enter a Name or \"-1\" to link to grades.");
+            name = input2.nextLine();
+            if (name.equals("-1")){
+                break;
+            }
+            names[count2] = name;
+            count2++;
+        }
+        for(int e = 0; e < count2 || e < count; e++) {
+            System.out.println(grades[e] + " : " + names[e]);
+        }
     }
 }
